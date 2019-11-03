701db7fbb9093202020fbc39a0a712fc3861e56b
Kerim <kerimcanislamoglu1@gmail.com>
2019-11-03 20:43:35 +0300

    DepixenFrontend
    Frontend Uygulaması Argus Medya

---
+++ DepixenFrontEnd/script.js
@@ -0,0 +1,36 @@
+function animateValue(id, start, end, duration) {
+    // assumes integer values for start and end
+    
+    var obj = document.getElementById(id);
+    var range = end - start;
+    // no timer shorter than 50ms (not really visible any way)
+    var minTimer = 50;
+    // calc step time to show all interediate values
+    var stepTime = Math.abs(Math.floor(duration / range));
+    
+    // never go below minTimer
+    stepTime = Math.max(stepTime, minTimer);
+    
+    // get current time and calculate desired end time
+    var startTime = new Date().getTime();
+    var endTime = startTime + duration;
+    var timer;
+  
+    function run() {
+        var now = new Date().getTime();
+        var remaining = Math.max((endTime - now) / duration, 0);
+        var value = Math.round(end - (remaining * range));
+        obj.innerHTML = value;
+        if (value == end) {
+            clearInterval(timer);
+        }
+    }
+    
+    timer = setInterval(run, stepTime);
+    run();
+}
+
+animateValue("counter1", 0, 132, 5000);
+animateValue("counterDecimal1", 0, 112, 5000);
+animateValue("counter2", 0, 11, 5000);
+animateValue("counterDecimal2", 0, 922, 5000);
